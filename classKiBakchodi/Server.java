755660241b7990c939114a297b8ae8a28955311e
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2024-11-23 04:59:59 +0530

    updated

---
+++ classKiBakchodi/Server.java
@@ -0,0 +1,5 @@
+package classKiBakchodi;
+
+public class Server {
+    
+}
--- oops/lec1.java
+++ oops/lec1.java
@@ -1,61 +1,32 @@
 package oops;
 
-class Pen{
-    String color;
-    String type;
-    String brand;
-    public void write(){
-        System.out.println("Pen is used to write");
-    }
-    public void printColor(){
-        System.out.println(this.color);
-    }
-    public void printBrand(){
-        System.out.println(this.brand);
-    }
-    public void penType(){
-        System.out.println(this.type);
-    }
-}
-
-class Student{
+class Dog{
     String name;
     int age;
+    String color;
 
-    public void getInfo(){
-        System.out.println("Your name is " + this.name );
-        System.out.println("Your age is " + this.age );
+    void walk(){
+        System.out.println( name + " the dog is walking");
     }
 
+    void bark(){
+        System.out.println("His age is " + age + " year and he is barking");
+    }
+
+    void sleep(){
+        System.out.println("His color is " + color + " and he is sleeping");
+    }
 }
 
-public class lec1 {
+public class lec1{
     public static void main(String[] args) {
+        Dog d1 = new Dog();
+        d1.name = "Candy";
+        d1.age = 1;
+        d1.color = "White";
 
-//! Pen Example
-        // Pen pen1 = new Pen();
-        // pen1.color = "Blue";
-        // pen1.type = "BallPoint";
-        // pen1.brand = "Reynolds";
-
-        // Pen pen2 = new Pen();
-        // pen2.color = "Black";
-        // pen2.type = "Gel";
-        // pen2.brand = "Butterflow"; 
-
-        // pen1.printColor();
-        // pen1.penType();
-        // pen1.printBrand();
-
-        // pen2.printColor();
-        // pen2.penType();
-        // pen2.printBrand();
-//! Student info
-
-        Student stu1 = new Student();
-        stu1.name = "Shivam kumar";
-        stu1.age = 20;
-
-        stu1.getInfo();
+        d1.walk();
+        d1.bark();
+        d1.sleep();
     }
 }
